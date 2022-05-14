bc6d731915ed9490450353cd3f7be64efc09c70c
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-05-14 18:32:05 +0200

    2D array demo

--- ArrayDemo.playground/Contents.swift
+++ ArrayDemo.playground/Contents.swift
@@ -1,14 +1,21 @@
 let perStudentPetCount = [1,2,0,3,2,0,0,0,1,2,2,3,4,0,0,1]
-
 let nmberOfStudnets = perStudentPetCount.count
-
-
 var totalNumberPets = 0
 
 for index in perStudentPetCount {
     totalNumberPets += index
 }
-print(totalNumberPets)
 
 let averagePets = totalNumberPets / nmberOfStudnets
 print(averagePets)
+
+
+print("-------------------------------------------")
+
+var order = [["Water", "Orange Juice"], ["Steak", "Rice", "Molten Cake"]]
+for(_, items) in order.enumerated() {
+    for item in items
+    {
+        print("\(item)")
+    }
+}
